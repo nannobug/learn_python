0de43995f5f2ad74110fa756b971dcfc6d4f9755
Dora Jiang <dora.nan.j@gmail.com>
2022-02-14 12:50:43 -0800

    declear, initialize, and use variables

---
+++ variables.py
@@ -0,0 +1,7 @@
+character_name = "John"
+character_age = "35"
+
+print("There once was a man named " + character_name + ", ")
+print("He is " + character_age + " years old. ")
+print("He loved the name of " + character_name + ", ")
+print("but he didn't like being " + character_age + ".")
